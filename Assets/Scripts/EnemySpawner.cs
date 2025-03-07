afd126ea86a48123366a1c2f36b2dedcb2efd483
Dani <danielferrert10@gmail.com>
2025-03-07 17:32:01 +0100

    velocidad aumenta

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,24 +7,56 @@ public class EnemySpawner : MonoBehaviour
     public GameObject balcon;
     public GameObject volador;
 
+    enemyController enemyBalcon;
+    enemyController enemyVolador;
+    public buildingController building;
+
     public Transform[] balcones;
+
+    public float tiempoBalcon;
+    public float tiempoVolador;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBalcon", 2, 2);
-        InvokeRepeating("SpawnVolador", 4, 4);
+        enemyBalcon = balcon.gameObject.GetComponent<enemyController>();
+        enemyVolador = volador.gameObject.GetComponent<enemyController>();
+
+        enemyBalcon.speed = 8f;
+        enemyVolador.speed = 10f;
+
+        InvokeRepeating("SpawnBalcon", tiempoBalcon, tiempoBalcon);
+        InvokeRepeating("SpawnVolador", tiempoVolador, tiempoVolador);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(enemyBalcon.speed >= 50f)
+        {
+            enemyBalcon.speed = 50f;
+        }
+
+        if(enemyVolador.speed >= 50f)
+        {
+            enemyVolador.speed = 50f;
+        }
+
+        if(building.speed >= 50f)
+        {
+            building.speed = 50f;
+        }
     }
 
     public void SpawnBalcon()
     {
         int pos = Random.Range(0, 1);
+
+        enemyBalcon.speed += .5f;
+
+        building.speed = enemyBalcon.speed;
+
+        tiempoBalcon -= .5f;
 
         if(pos == 0)
         {
@@ -39,6 +71,10 @@ public class EnemySpawner : MonoBehaviour
     public void SpawnVolador()
     {
         float pos = Random.Range(6f, -6f);
+
+        enemyVolador.speed += .5f;
+
+        tiempoVolador -= .5f;
 
         Vector2 spawner = new Vector2(pos, transform.position.y);
         
